4e154105440a386173e1c385c63493e9f1920b7a
Nigel Horne <njh@bandsman.co.uk>
2025-01-09 08:05:43 -0500

    Release 0.02

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Test::Mockingbird - Advanced mocking library for Perl with support for dependenc
 
 # VERSION
 
-Version 0.01
+Version 0.02
 
 # SYNOPSIS
 
--- lib/Test/Mockingbird.pm
+++ lib/Test/Mockingbird.pm
@@ -14,11 +14,11 @@ Test::Mockingbird - Advanced mocking library for Perl with support for dependenc
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
